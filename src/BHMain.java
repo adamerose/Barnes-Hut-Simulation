27fab92186b919e41d4505e3d890c5d77476414d
esoremada <esoremada@gmail.com>
2016-01-11 19:27:08 -0500

    First commit

--- src/BHMain.java
+++ src/BHMain.java
@@ -24,134 +24,134 @@ import javax.swing.Timer;
  */
 
 public class BHMain{
-	public static final boolean drawQuads = false; // Render Barnes Hut quads or just particles
-	public static final int DT = 10; 		// Time-step in milliseconds
-	public static final int WIDTH = 800;	// Dimensions of JPanel
-	public static final int HEIGHT = WIDTH; // Algorithm assumes a square universe
-	private ArrayList<Body> bodies = new ArrayList<>();	// Stores all bodies
-	private Quad root;		// Root node of the Barnes Hut quad-tree
-	private MyPanel panel;	// Bodies are drawn to this overridden JPanel
-	private JFrame frame;	// Contains the JPanel
-	private long time;		// Used to calculate the fps in calcFrameRate()
-	
-	public static void main(String[] args) {
-		BHMain sim = new BHMain();
-	}
-	
-	public BHMain(){
-		// Create and initialize GUI
-		panel = new MyPanel();
-		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		frame = new JFrame();
-		frame.add(panel);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-		time = System.currentTimeMillis();
-		
-		// Create starting bodies and build the initial Barnes Hut tree
-		addDefaultBodies(500, 3);
-		buildBarnesHutTree();
+    public static final boolean drawQuads = false; // Render Barnes Hut quads or just particles
+    public static final int DT = 10;        // Time-step in milliseconds
+    public static final int WIDTH = 800;    // Dimensions of JPanel
+    public static final int HEIGHT = WIDTH; // Algorithm assumes a square universe
+    private ArrayList<Body> bodies = new ArrayList<>();    // Stores all bodies
+    private Quad root;        // Root node of the Barnes Hut quad-tree
+    private MyPanel panel;    // Bodies are drawn to this overridden JPanel
+    private JFrame frame;     // Contains the JPanel
+    private long time;        // Used to calculate the fps in calcFrameRate()
+    
+    public static void main(String[] args) {
+        BHMain sim = new BHMain();
+    }
+    
+    public BHMain(){
+        // Create and initialize GUI
+        panel = new MyPanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        frame = new JFrame();
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        time = System.currentTimeMillis();
+        
+        // Create starting bodies and build the initial Barnes Hut tree
+        addDefaultBodies(500, 3);
+        buildBarnesHutTree();
 
-		// Start the simulation
-		Timer myTimer = new Timer(DT, new TimerListener());
-		myTimer.start();
-	}
-	
-	/*
-	 * Creates n randomly distributed particles in a square area with a margin
-	 * of empty space around the edge of the JPanel
-	 * 
-	 */
-	private void addDefaultBodies(int n, float m){
-		double margin = 0.2;
-		Random rand = new Random();
-		for(int i=0; i<n; i++){
-			double x = HEIGHT*(margin + (1-2*margin)*rand.nextFloat());
-			double y = WIDTH*(margin + (1-2*margin)*rand.nextFloat());
-			//Give the particles a circular motion around the center of the frame
-			double vx = 1*(y-HEIGHT/2);
-			double vy = -1*(x-WIDTH/2);
-			
-			Body p = new Body((int)x,(int)y,vx,vy,m);
-			bodies.add(p);
-		}
-	}
-	
-	/*
-	 * Build a Barnes Hut quad tree containing all the bodies in the bodies
-	 * array with the root node as its head
-	 */
-	private void buildBarnesHutTree(){
-		//The root will be a square quad with its length equal to the JPanel
-		root = new Quad(WIDTH/2, HEIGHT/2, WIDTH, 0);
-		for(Body b : bodies){
-			root.insert(b);
-		}
-	}
-	
-	/*
-	 * Update the position and velocity of all bodies for the current frame
-	 * then build a new Barnes Hut tree with the updated particles
-	 */
-	private void update(){
-		for(Body b : bodies){
-			b.updatePosition();
-			root.updateVelocityOf(b);
-		}
-		buildBarnesHutTree();
-	}
-	
-	/*
-	 * Returns the frame rate of the simulation by dividing by the 
-	 * time between the current and previous frame
-	 */
-	private int calcFrameRate(){
-		long newTime = System.currentTimeMillis();
-		long fps = 1000/(newTime - time);
-		time = newTime;
-		return Math.round(fps);
-	}
-	
-	/*
-	 * Updates and renders bodies every frame
-	 * 
-	 * The time interval between frames is defined by the constant 
-	 * DT used to create myTimer in BHMain()
-	 * 
-	 */
-	private class TimerListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			update();
-			frame.repaint();
-			int fps = calcFrameRate();
-			frame.setTitle("Body Simulation | " + fps + " fps | " + bodies.size() + " bodies");
-		}
-	}
-	
-	/*
-	 * Renders the JPanel when repaint is called every frame
-	 */
-	private class MyPanel extends JPanel{
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			Graphics2D g2d = (Graphics2D) g;
+        // Start the simulation
+        Timer myTimer = new Timer(DT, new TimerListener());
+        myTimer.start();
+    }
+    
+    /*
+     * Creates n randomly distributed particles in a square area with a margin
+     * of empty space around the edge of the JPanel
+     * 
+     */
+    private void addDefaultBodies(int n, float m){
+        double margin = 0.2;
+        Random rand = new Random();
+        for(int i=0; i<n; i++){
+            double x = HEIGHT*(margin + (1-2*margin)*rand.nextFloat());
+            double y = WIDTH*(margin + (1-2*margin)*rand.nextFloat());
+            //Give the particles a circular motion around the center of the frame
+            double vx = 1*(y-HEIGHT/2);
+            double vy = -1*(x-WIDTH/2);
+            
+            Body p = new Body((int)x,(int)y,vx,vy,m);
+            bodies.add(p);
+        }
+    }
+    
+    /*
+     * Build a Barnes Hut quad tree containing all the bodies in the bodies
+     * array with the root node as its head
+     */
+    private void buildBarnesHutTree(){
+        //The root will be a square quad with its length equal to the JPanel
+        root = new Quad(WIDTH/2, HEIGHT/2, WIDTH, 0);
+        for(Body b : bodies){
+            root.insert(b);
+        }
+    }
+    
+    /*
+     * Update the position and velocity of all bodies for the current frame
+     * then build a new Barnes Hut tree with the updated particles
+     */
+    private void update(){
+        for(Body b : bodies){
+            b.updatePosition();
+            root.updateVelocityOf(b);
+        }
+        buildBarnesHutTree();
+    }
+    
+    /*
+     * Returns the frame rate of the simulation by dividing by the 
+     * time between the current and previous frame
+     */
+    private int calcFrameRate(){
+        long newTime = System.currentTimeMillis();
+        long fps = 1000/(newTime - time);
+        time = newTime;
+        return Math.round(fps);
+    }
+    
+    /*
+     * Updates and renders bodies every frame
+     * 
+     * The time interval between frames is defined by the constant 
+     * DT used to create myTimer in BHMain()
+     * 
+     */
+    private class TimerListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            update();
+            frame.repaint();
+            int fps = calcFrameRate();
+            frame.setTitle("Body Simulation | " + fps + " fps | " + bodies.size() + " bodies");
+        }
+    }
+    
+    /*
+     * Renders the JPanel when repaint is called every frame
+     */
+    private class MyPanel extends JPanel{
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
 
-			// Redefines coordinates so that 0,0 is at the bottom left
-			g2d.scale(1, -1);
-			g2d.translate(0, -getHeight());
+            // Redefines coordinates so that 0,0 is at the bottom left
+            g2d.scale(1, -1);
+            g2d.translate(0, -getHeight());
 
-			// Turn on anti-aliasing
-			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            // Turn on anti-aliasing
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-			// Draw all Bodies in Body array
-			for (Body p : bodies) {
-				p.drawBody(g2d);
-			}
+            // Draw all Bodies in Body array
+            for (Body p : bodies) {
+                p.drawBody(g2d);
+            }
 
-			// Draw Barnes Hut quads
-			if (drawQuads == true)
-				root.drawAll(g);
-		}
-	}
+            // Draw Barnes Hut quads
+            if (drawQuads == true)
+                root.drawAll(g);
+        }
+    }
 }
--- src/Body.java
+++ src/Body.java
@@ -1,82 +1,82 @@
 import java.awt.Graphics2D;
 
 public class Body {
-	double DT = BHMain.DT/1000.0; 	 // Time step in seconds
-	private final double G = 1E5;	 // Gravity strength
-	private final double DAMP = 100; // Damping strength used in updatePosition()
-	private double xPos, yPos;
-	private double xVel, yVel;
-	private double radius;
-	private double mass;
-	
-	
-	public Body(double xPos, double yPos, double xVel, double yVel, double radius) {
-		this.xPos = xPos;
-		this.yPos = yPos;
-		this.xVel = xVel;
-		this.yVel = yVel;
-		this.radius = radius;
-		this.mass = Math.PI * radius * radius; // Mass proportional to area
-	}
+    double DT = BHMain.DT/1000.0;      // Time step in seconds
+    private final double G = 1E5;     // Gravity strength
+    private final double DAMP = 100; // Damping strength used in updatePosition()
+    private double xPos, yPos;
+    private double xVel, yVel;
+    private double radius;
+    private double mass;
+    
+    
+    public Body(double xPos, double yPos, double xVel, double yVel, double radius) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xVel = xVel;
+        this.yVel = yVel;
+        this.radius = radius;
+        this.mass = Math.PI * radius * radius; // Mass proportional to area
+    }
 
-	/*
-	 * Combines this body with another body and returns the aggregate body.
-	 * 
-	 * Velocity is not calculated because position will be updated as the
-	 * contained bodies are updated individually.
-	 */
-	public Body combine(Body b) {
-		double m = mass + b.mass;
-		double r = Math.sqrt(m / (Math.PI));
-		double x = (xPos * mass + b.xPos * b.mass) / m;
-		double y = (yPos * mass + b.yPos * b.mass) / m;
-		return new Body(x, y, 0, 0, r);
-	}
+    /*
+     * Combines this body with another body and returns the aggregate body.
+     * 
+     * Velocity is not calculated because position will be updated as the
+     * contained bodies are updated individually.
+     */
+    public Body combine(Body b) {
+        double m = mass + b.mass;
+        double r = Math.sqrt(m / (Math.PI));
+        double x = (xPos * mass + b.xPos * b.mass) / m;
+        double y = (yPos * mass + b.yPos * b.mass) / m;
+        return new Body(x, y, 0, 0, r);
+    }
 
-	/*
-	 * Draw this body as a circle centered on xPos and yPos
-	 */
-	void drawBody(Graphics2D g2d) {
-		int diameter = (int) (2 * radius);
-		int x = (int) (xPos - radius);
-		int y = (int) (yPos - radius);
-		g2d.fillOval(x, y, diameter, diameter);
-	}
-	
-	/*
-	 * Updates the position change of this body due to its velocity and
-	 * velocity change of this body due to force from Body b
-	 */
-	void updatePosition() {
-		xPos += DT*xVel;
-		yPos += DT*yVel;
-	}
-	
-	void updateVelocity(Body b) {
-		double EPS = DAMP*radius;
-		
-		// Force on this body
-		double dx = xPos - b.xPos;
-		double dy = yPos - b.yPos;
-		double r = Math.sqrt((dx*dx + dy*dy));
-		
-		// Softened Newtonian force to eliminate erratic behavior on collisions
-		double F = - G * mass * b.mass / (r*r + EPS*EPS);
-		double angle = Math.atan2(dy, dx);
-		double Fx = F*Math.cos(angle);
-		double Fy = F*Math.sin(angle);
+    /*
+     * Draw this body as a circle centered on xPos and yPos
+     */
+    void drawBody(Graphics2D g2d) {
+        int diameter = (int) (2 * radius);
+        int x = (int) (xPos - radius);
+        int y = (int) (yPos - radius);
+        g2d.fillOval(x, y, diameter, diameter);
+    }
+    
+    /*
+     * Updates the position change of this body due to its velocity and
+     * velocity change of this body due to force from Body b
+     */
+    void updatePosition() {
+        xPos += DT*xVel;
+        yPos += DT*yVel;
+    }
+    
+    void updateVelocity(Body b) {
+        double EPS = DAMP*radius;
+        
+        // Force on this body
+        double dx = xPos - b.xPos;
+        double dy = yPos - b.yPos;
+        double r = Math.sqrt((dx*dx + dy*dy));
+        
+        // Softened Newtonian force to eliminate erratic behavior on collisions
+        double F = - G * mass * b.mass / (r*r + EPS*EPS);
+        double angle = Math.atan2(dy, dx);
+        double Fx = F*Math.cos(angle);
+        double Fy = F*Math.sin(angle);
 
-		// Update velocity. a = F / m.  v = v0 + a*t
-		xVel += DT * Fx / mass;
-		yVel += DT * Fy / mass; 
-	}
-	
+        // Update velocity. a = F / m.  v = v0 + a*t
+        xVel += DT * Fx / mass;
+        yVel += DT * Fy / mass; 
+    }
+    
 
-	double getX(){
-		return xPos;
-	}
-	
-	double getY(){
-		return yPos;
-	}
+    double getX(){
+        return xPos;
+    }
+    
+    double getY(){
+        return yPos;
+    }
 }
